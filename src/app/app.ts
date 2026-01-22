919c84fe49975d94364aa2be5b1d5b9f2b3283d8
Omnithrix <OKaraca@gmx.de>
2026-01-22 12:54:14 +0100

    Issues #2 to #5

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Header } from "./header/header";
+import { Header } from './header/header';
+import { Filter } from './filter/filter';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Filter],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todoApp');
